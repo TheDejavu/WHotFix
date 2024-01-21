d70674e030cd8ef19dffd0124e3a346190aec384
TheDejavu <thedeejayv1@gmail.com>
2024-01-21 03:17:29 +0200

    made y threshold configurable

--- src/main/java/dev/orhidea/whotfix/WHotFix.java
+++ src/main/java/dev/orhidea/whotfix/WHotFix.java
@@ -5,27 +5,31 @@ import org.bukkit.Location;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Wither;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class WHotFix extends JavaPlugin implements Listener {
 
+    private int yThreshold;
+
     @Override
     public void onEnable() {
+        saveDefaultConfig();
+        reloadConfig();
+        yThreshold = getConfig().getInt("yThreshold", -1);
         this.getServer().getPluginManager().registerEvents(this, this);
     }
 
     @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-    }
+    public void onDisable() {}
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onWitherMove(EntityMoveEvent event){
         Entity entity = event.getEntity();
         if (entity instanceof Wither){
             Location location = event.getTo();
-            if (location.getY() <= -1)
+            if (location.getBlockY() <= yThreshold)
                 entity.remove();
         }
     }
