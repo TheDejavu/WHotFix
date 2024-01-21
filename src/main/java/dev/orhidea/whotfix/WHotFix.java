4110bf814ab54a0d9908e3b9507f3120789cea35
TheDejavu <thedeejayv1@gmail.com>
2024-01-21 02:19:20 +0200

    init

--- src/main/java/dev/orhidea/whotfix/WHotFix.java
+++ src/main/java/dev/orhidea/whotfix/WHotFix.java
@@ -1,17 +1,32 @@
 package dev.orhidea.whotfix;
 
+import io.papermc.paper.event.entity.EntityMoveEvent;
+import org.bukkit.Location;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Wither;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class WHotFix extends JavaPlugin {
+public final class WHotFix extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        this.getServer().getPluginManager().registerEvents(this, this);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    @EventHandler
+    public void onWitherMove(EntityMoveEvent event){
+        Entity entity = event.getEntity();
+        if (entity instanceof Wither){
+            Location location = event.getTo();
+            if (location.getY() <= -1)
+                entity.remove();
+        }
     }
 }
