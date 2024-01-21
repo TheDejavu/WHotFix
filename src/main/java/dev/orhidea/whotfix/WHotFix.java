b7b9ea155d85cae7d77b3051d6fdab1b25f52000
TheDejavu <thedeejayv1@gmail.com>
2024-01-21 02:16:45 +0200

    Initial commit

---
+++ src/main/java/dev/orhidea/whotfix/WHotFix.java
@@ -0,0 +1,17 @@
+package dev.orhidea.whotfix;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WHotFix extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
